ced72b6f4f45c4ec8220ff4019bfc5f7cbe34fd8
JuliusDorfman <juliusdorfman@gmail.com>
2017-09-01 18:59:19 -0700

    .js nearly complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,6 +24,9 @@ $(document).ready(function() {
         answers: []
     };
 
+    var correctAnswers = 0;
+    var wrongAnswers = 0;
+
     $('#startBtn')
         .click(function() {
             init();
@@ -36,13 +39,17 @@ $(document).ready(function() {
     }
 
     function roundGenerator(i) {
-        if (i <= 0) return;
-        setTimeout(function() {
+        if (i > 0) {
             questionsGenerator(i);
-            answersGenerator();
-            choicesPusher(i);
-            roundGenerator(--i);
-        }, 800);
+            answersGenerator(i);
+            buttonAssigner(i);
+            buttonHandler(i);
+            setTimeout(function() {
+                roundGenerator(--i);
+            }, 1200);
+        } else {
+            endScreen();
+        }
     };
 
     function questionsGenerator(i) {
@@ -54,49 +61,128 @@ $(document).ready(function() {
         return Math.floor(Math.random() * (interactiveBox.choices.length))
     }
 
-    function answersGenerator() {
+    function answersGenerator(i) {
         interactiveBox.answers = [];
-        answerNum1 = randomNumber();
+        answerNum1 = interactiveBox.choices[interactiveBox.choices.length - i];
+        (interactiveBox.answers).push(answerNum1);
+        answerNum1String = interactiveBox.choices.indexOf(answerNum1);
+        console.log('', answerNum1);
         answerNum2 = randomNumber();
         answerNum3 = randomNumber();
         answerNum4 = randomNumber();
-        console.log('', answerNum1);
-        console.log('', answerNum2);
-        console.log('', answerNum3);
-        console.log('', answerNum4);
-        if ((answerNum1 !== answerNum2) && (answerNum1 !== answerNum3) &&
-            (answerNum1 !== answerNum4) && (answerNum2 !== answerNum3) &&
+        // REPLACE CODE BELOW WITH FUNCTION TO SEARCH THROUGH NEW ARRAY AND REPLACE THE DUPLICATES
+        if ((answerNum1String !== answerNum2) && (answerNum1String !== answerNum3) &&
+            (answerNum1String !== answerNum4) && (answerNum2 !== answerNum3) &&
             (answerNum2 !== answerNum4) && (answerNum3 !== answerNum4)) {
-            (interactiveBox.answers).push(interactiveBox.choices[answerNum1]);
             (interactiveBox.answers).push(interactiveBox.choices[answerNum2]);
             (interactiveBox.answers).push(interactiveBox.choices[answerNum3]);
             (interactiveBox.answers).push(interactiveBox.choices[answerNum4]);
-            console.log('', interactiveBox.answers);
         } else {
-            answersGenerator();
+            answersGenerator(i);
+        }
+        shuffleAnswers(interactiveBox.answers);
+    }
+
+    function shuffleAnswers(array) {
+        var i = 0,
+            j = 0,
+            temp = null
+
+        for (i = array.length - 1; i > 0; i -= 1) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
     }
 
-    function choicesPusher(i) {
+    function buttonAssigner(i) {
         $('.answers').html(
             $('<container>')
             .append(
-                $('<div>').text(interactiveBox.choices[interactiveBox.choices.length - i]))
+                $('<div>')
+                .text(interactiveBox.answers[0])
+                .addClass('btn btn-block userChoice')
+                .val(interactiveBox.answers[0]))
             .append(
-                $('<div>').text(interactiveBox.answers[0]))
+                $('<div>')
+                .text(interactiveBox.answers[1])
+                .addClass('btn btn-block userChoice')
+                .val(interactiveBox.answers[1]))
             .append(
-                $('<div>').text(interactiveBox.answers[1]))
+                $('<div>')
+                .text(interactiveBox.answers[2])
+                .addClass('btn btn-block userChoice')
+                .val(interactiveBox.answers[2]))
             .append(
-                $('<div>').text(interactiveBox.answers[2]))
+                $('<div>')
+                .text(interactiveBox.answers[3])
+                .addClass('btn btn-block userChoice')
+                .val(interactiveBox.answers[3]))
         );
     }
 
+    function buttonHandler(i) {
+        var answerMatch = false;
+        var hasChosen = 0;
+        $('.userChoice').on('click', function() {
+            if (hasChosen == 0) {
+                hasChosen = 1
+                var userPicked = $(this).prop('value')
+                if (userPicked == answerNum1) {
+                    answerMatch = true;
+                    console.log('', answerMatch);
+                    winScreen(userPicked);
+                } else {
+                    console.log('', answerMatch);
+                    loseScreen(userPicked);
+                }
+            }
+        });
+    }
 
+    function winScreen(userPicked) {
+        $('.question-asked').empty();
+        $('.answers').empty();
+        $('.question-asked')
+            .html('Answer: ' + userPicked)
+        winCounter();
+        console.log('', correctAnswers);
+    }
+
+    function loseScreen(userPicked) {
+        $('.question-asked').empty();
+        $('.answers').empty();
+        $('.question-asked')
+            .text('Incorrect!')
+            .text('Answer: ' + userPicked)
+        loseCounter();
+        console.log('', wrongAnswers);
+    }
+
+    function winCounter() {
+        correctAnswers += 1;
+    }
+
+    function loseCounter() {
+        wrongAnswers += 1;
+    }
+
+    function endScreen() {
+        $('.question-asked').empty();
+        $('.answers').empty();
+        $('.question-asked')
+            .append(
+                $('<div>')
+                .text('You answered ' + correctAnswers + ' questions correctly!'))
+            .append(
+                $('<div>')
+                .text('You got ' + wrongAnswers + ' incorrect!'))
+        return
+    }
 });
 
 
-
-// USE .appending
 // Stopwatching
 
 
